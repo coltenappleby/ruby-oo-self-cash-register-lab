05e204a157f17ce55f46fd6301ee8c9f1bfcd731
Colten Appleby <colten.appleby@gmail.com>
2021-02-24 23:51:55 -0500

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,36 @@
+require "pry"
 
+class CashRegister
+
+    attr_accessor :total, :items
+    attr_reader :total, :discount
+
+
+    def initialize(employee_discount = 0)
+        @total = 0
+        @discount = employee_discount
+        @items = []
+    end
+
+    def add_item(item, cost, quantity = 1)
+        self.total += (cost * quantity)
+        while quantity > 0
+            self.items << item
+            quantity -= 1
+        end
+    end
+
+    def apply_discount
+        #binding.pry
+        if self.discount == 0
+            return "There is no discount to apply."
+        end
+        self.total *= (1-self.discount/100.0)
+        return "After the discount, the total comes to $#{self.total.to_i}."
+    end
+
+
+    
+
+
+end
